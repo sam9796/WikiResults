4f8283cd49077ea11991e9eb5fc3b2f921553925
Shubham Jha <jhashubham976@gmail.com>
2024-01-19 01:34:33 +0530

    First Commit

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
     <nav className='bg-slate-600 p-4 flex justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl'>
       <h1 className='text-3xl font-bold text-white grid plcae-content-center mb-2 md:mb-0'>
         <Link href='/'>
-            WikiRocket!
+            WikiResults!
         </Link>
       </h1>
       <Search/>
